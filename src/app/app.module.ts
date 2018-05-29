2e06d0cc7a85bda06df8ae8fc5ec9b42f13f06a0
Christian Skar <skarchr@gmail.com>
2018-05-29 09:02:59 +0200

    Offline version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -16,7 +18,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
     BrowserModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CatImage } from '../classes/cat-image';
+import { Button } from '../classes/Button';
 
 @Component({
   selector: 'app-img-card',
@@ -15,11 +16,21 @@ export class ImgCardComponent implements OnInit {
   };
   
   public src:string;
+  public button:Button = {
+    text:'Give me another cat',
+    color:'primary',
+    disabled:false
+  };
 
   constructor() { }
 
   ngOnInit() {
-    this.src = this.image.api + this.image.message;
+    this.src = this.image.api + this.image.message + 
+      '?size=' + this.image.fontsize;
+    if (!navigator.onLine) {
+      this.button.text = 'Sorry, you\'re offline';
+      this.button.disabled = true;
+    }
   }
 
   generateSrc():void {
